b9444243a7185af2e7ebee90f3752fe3efc62a5d
Matt Higgins <higginsmt@gmail.com>
2014-02-20 10:42:10 -0500

    1-3 working sweeeet

--- spec/quizSpec.js
+++ spec/quizSpec.js
@@ -12,12 +12,12 @@
 
 describe('question1', function() {
 
-  it('returns true if today is saturday or sunday', function() {
+  it('.sleepIn returns true if today is saturday or sunday', function() {
     // Assuming you're taking this on a weekday!
     expect(quiz.sleepIn()).toBe(false);
   });
 
-  it('returns true if an options hash is passed with vacation set to true', function() {
+  it('.sleepIn returns true if an options hash is passed with vacation set to true', function() {
     expect(quiz.sleepIn({vacation: true})).toBe(true);
   });
 
@@ -25,3 +25,35 @@ describe('question1', function() {
 
 // Write the next tests yourself!
 // See quiz.js for more details
+
+describe('question2', function() {
+
+  it('.nearHundred returns true if the number is between 90 and 99', function() {
+    expect(quiz.nearHundred(90)).toBe(true);
+  });
+
+  it('.nearHundred returns false if the number is not between 90 and 99', function() {
+    expect(quiz.nearHundred(100)).toBe(false);
+  });
+
+});
+
+
+describe('question3', function() {
+
+  it('.missingChar removes a character that corresponds to the index from the string', function() {
+    expect(quiz.missingChar("pokemon", 1)).toEqual("pkemon");
+  });
+
+  it(".missingChar gives you an error if you don't feed it a string", function() {
+    expect(quiz.missingChar(347, 1)).toEqual("Error: Please enter a string!");
+  });
+
+});
+
+
+
+
+
+
+
