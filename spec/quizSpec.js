ca440285c4fa8ad6894d96236140efa44c63958c
Yiyang Song <songyiyang2012@gmail.com>
2014-02-20 10:39:33 -0500

    Quiz done

--- spec/quizSpec.js
+++ spec/quizSpec.js
@@ -23,5 +23,60 @@ describe('question1', function() {
 
 });
 
-// Write the next tests yourself!
-// See quiz.js for more details
+describe('question2', function() {
+
+  it('returns true if number is near hundred', function() {
+    // Assuming you're taking this on a weekday!
+    expect(quiz.nearHundred(93)).toBe(true);
+  });
+
+  it('returns false if number is not near hundred', function() {
+    expect(quiz.nearHundred(10)).toBe(false);
+  });
+  it('raise error if not pass number', function(){
+  			expect(function(){
+ 				quiz.nearHundred('two')
+ 			}).toThrowError("Please enter a number!");
+  });
+});
+
+
+describe('question3', function() {
+
+  it('returns character removed corresponds to the index', function() {
+    // Assuming you're taking this on a weekday!
+    expect(quiz.missingChar("kittie", 1)).toEqual("kttie");
+  });
+
+  it('raise error if not pass string', function(){
+  			expect(function(){
+ 				quiz.missingChar(347, 1)
+ 			}).toThrowError("Please enter a string!");
+  });
+});
+
+describe('question4', function() {
+
+  it('remove del from a string', function() {
+    
+    expect(quiz.delDel("abdelcd")).toEqual("abcd");
+  });
+  it('remove nothing from a string if no del', function() {
+
+    expect(quiz.delDel("xyz")).toEqual("xyz");
+  });
+  
+});
+
+describe('question5', function() {
+
+  it('move last char to beginning', function() {
+  	expect("hello".backAround()).toEqual("ohell");
+    expect("cat".backAround()).toEqual("tca");
+  });
+  
+  
+});
+
+
+
