ec6eb5fb6b9567419fc0fdf95637c0a9b1bd3b4e
David Fisher <tibbon@gmail.com>
2014-02-20 10:35:02 -0500

    Changed runner setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,7 @@ module.exports = function(grunt) {
         ],
         options: {
           specs: 'spec/*Spec.js',
-          keepRunner : true
+          keepRunner : false
         }
       }
     }
