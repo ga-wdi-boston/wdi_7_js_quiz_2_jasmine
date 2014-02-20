773f7d8c5e8058211b898b7d14ba52fd919e3de1
Prescott Murphy <prurph@gmail.com>
2014-02-20 10:23:51 -0500

    It's coming .backAround() again

--- spec/quizSpec.js
+++ spec/quizSpec.js
@@ -25,3 +25,49 @@ describe('question1', function() {
 
 // Write the next tests yourself!
 // See quiz.js for more details
+describe('question2: quiz.nearHundred()', function() {
+  it('returns true if the number is between 90 and 99', function() {
+    expect(quiz.nearHundred(90)).toBe(true);
+    expect(quiz.nearHundred(99)).toBe(true);
+  });
+  it('returns false if the number is 89 or below', function() {
+    expect(quiz.nearHundred(89)).toBe(false);
+    expect(quiz.nearHundred(5)).toBe(false);
+  });
+  it('throws an error if the argument is not a number', function() {
+    expect(function() { quiz.nearHundred('mango'); }).toThrowError("Please enter a number!");
+  });
+});
+
+describe('question3: quiz.missingChar()', function() {
+  it('throws an error if argument is not a string', function() {
+    expect(function() { quiz.missingChar(347, 1); }).toThrowError("Please enter a string!");
+  });
+  it('removes the character that corresponds to the index from the string and returns string',
+    function() {
+    expect(quiz.missingChar("kittie", 1)).toEqual("kttie");
+    expect(quiz.missingChar("Alex", 3)).toEqual("Ale");
+  });
+});
+
+describe('question4: quiz.delDel()', function() {
+  it('removes del from a string each time it appears therein', function() {
+    expect(quiz.delDel("abdelcd")).toEqual("abcd");
+    expect(quiz.delDel("abdelcddel")).toEqual("abcd");
+  });
+  it('returns the string untouched if it contains no del', function() {
+    expect(quiz.delDel("xyz")).toEqual("xyz");
+    expect(quiz.delDel("deDELel")).toEqual("deDELel");
+  });
+});
+
+describe('question5: String.backAround()', function() {
+  it('is a class methond on String', function() {
+    expect(String.prototype.hasOwnProperty('backAround')).toBe(true);
+    expect(Number.prototype.hasOwnProperty('backAround')).toBe(false);
+  });
+  it('moves the last character of a string to the beginning', function() {
+    expect("cat".backAround()).toEqual("tca");
+    expect("hello".backAround()).toEqual("ohell");
+  });
+});
--- src/quiz.js
+++ src/quiz.js
@@ -12,7 +12,14 @@ var quiz = {};
 // quiz.sleepIn() => false
 // quiz.sleepIn({vacation: true}) => true
 quiz.sleepIn = function(options) {
-}
+  var options    = options || {},
+      today      = new Date().getDay(),
+      getToSleep = false;
+  if (today === 0 || today === 6 || options.vacation === true) {
+      getToSleep = true;
+  }
+  return getToSleep;
+};
 
 // Question 2: function called nearHundred
 //////////////
@@ -21,6 +28,16 @@ quiz.sleepIn = function(options) {
 // quiz.nearHundred(52) => false
 // quiz.nearHundred(93) => true
 // quiz.nearHundred('two') => Error: Please enter a number!
+quiz.nearHundred = function(num) {
+  if (typeof num !== "number") {
+    throw new Error("Please enter a number!");
+  }
+  if (90 <= num && num <= 99) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 // Question 3: a function called missingChar
 //////////////
@@ -28,15 +45,27 @@ quiz.sleepIn = function(options) {
 // If you don't enter a string
 // quiz.missingChar("kittie", 1) => "kttie"
 // quiz.missingChar(347, 1) => Error: Please enter a string!
+quiz.missingChar = function(str, indx) {
+  if (typeof str !== "string") {
+    throw new Error("Please enter a string!");
+  }
+  return str.slice(0,indx) + str.slice(indx+1);
+};
 
 // Question 4: a function called delDel
 //////////////
 // Remove "del" from a string.
 // quiz.delDel("abdelcd") => "abcd"
 // quiz.delDel("xyz") => "xyz"
+quiz.delDel = function(str) {
+  return str.split("del").join("");
+}
 
 // Question 5: a method called backAround
 //////////////
 // Given a string, move the last character to the beginning.
 // "cat".backAround() => "tca"
 // "hello".backAround() => "ohell"
+String.prototype.backAround = function() {
+  return this.slice(-1) + this.slice(null,-1);
+}
